a2d9a5c2220d0ef880fb4a798a5433f95f9a9a07
Beni1211 <137344652+Beni1211@users.noreply.github.com>
2024-11-09 15:31:58 +0100

    Delete JS.js

--- JS.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert("Vielen Dank für deine Nachricht!");
-    this.reset();
-});
